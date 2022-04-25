c09879cc9b34d63f6b6f8d38b89a54b0b4df1465
DdDaza22 <djdaza95@gmail.com>
2022-04-25 13:15:36 +0200

    programa2

---
+++ holamundo.py
@@ -0,0 +1,17 @@
+#comentario de python
+
+saludo = "hola mundo"
+
+textdoslineas = """hola amigos
+hola de nuevo"""
+
+numero = 10
+Numero = 20
+
+print(numero)
+print(Numero)
+print(numero + Numero)
+print("saludo"+saludo)
+print(textdoslineas)
+print(type(numero))
+print(type(saludo))
---
+++ segprog.py
@@ -0,0 +1,11 @@
+a=5
+b=10
+print("Paso 1. V iniciales")
+print(a)
+print(b)
+c=b
+b=a
+a=c
+print("Paso 1. V finales")
+print(a)
+print(b)
